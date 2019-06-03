c25a8d43c415833467970843c59ba57646f6786b
Mebu/YukiNarayama <mebumitiru@gmail.com>
2019-06-03 20:22:22 +0900

    初期コミット　ちょっと作ってみた

---
+++ Assets/Script/Mogura.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mogura : MonoBehaviour
+{
+
+    //デバック用はあとでモグラマネージャー作って分ける
+    private int randomCount;//デバック用
+    public int randomCountMax=200;//デバック用
+    private Animator animator ;
+    void Start()
+    {
+        animator =this.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        randomCount=Random.Range(1,randomCountMax);//デバック用
+        if(randomCount==50)MoguraOut();//デバック用
+        Debug.Log(randomCount);//デバック用
+    }
+
+    void MoguraOut()
+    {
+        animator.SetTrigger("MoguraStart");
+    }
+    
+    void OnTriggerEnter()
+    {
+        animator.SetTrigger("Attack");
+    }
+}
---
+++ Assets/Script/RollStick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollStick : MonoBehaviour
+{
+    private　 Transform transForm;
+    //private Rigidbody rb;
+    public float moveSpeed = 1;
+    void Start ()
+    {
+        transForm = this.GetComponent<Transform> ();
+        //rb = this.GetComponent<Rigidbody> ();
+    }
+    void Update ()
+    {
+        if (Input.GetKey (KeyCode.Mouse0))
+        {
+            if (transForm.localEulerAngles.x < 90)
+            {
+            transForm.Rotate (new Vector3 (moveSpeed, 0, 0));
+            }
+            //if (transForm.localEulerAngles.x > 100) transForm.localEulerAngles = (new Vector3 (90f, 0, 0));
+            //rb.angularVelocity = new Vector3 (1.0f, 0, 0) * moveSpeed;
+        }
+        if (Input.GetKeyUp (KeyCode.Mouse0)) transForm.localEulerAngles = Vector3.zero;
+    }
+}
