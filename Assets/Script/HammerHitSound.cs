04fa8458175c28ef0dcb6dc4ed9bea9621a7dae8
Unknown <murauehi@icloud.com>
2019-06-06 13:26:44 +0900

    あぁ～^モグラをいぢめる音ぉ～^
    以下のものを追加
    ・モグラをいぢめる仮音
    ・モグラをいぢめるたら音が鳴るようにするスクリプト
    以下のものを変更
    ・モグラprefab (Audio Source Componentの追加)
    ・モグラprefab (スクリプトの適用)

---
+++ Assets/Script/HammerHitSound.cs
@@ -0,0 +1,20 @@
+﻿//もぐらをハンマーでいぢめると音が鳴るよ
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerHitSound : MonoBehaviour{
+    private AudioSource hitSuccess;
+
+    void Start(){
+        hitSuccess = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter (Collider other){
+        if(other.gameObject.CompareTag ("Hammer")){
+            hitSuccess.Play();
+            //Debug.Log ("Hit");                        //ログを出したい気分になったらコメント外してね
+        }
+    }
+}
