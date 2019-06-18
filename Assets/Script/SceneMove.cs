5198f12ae46d12d2645ccbdae16f8ec6b9ae8dba
Ryo Hashiguchi <m0118206a2@edu.teu.ac.jp>
2019-06-18 13:41:26 +0900

    シーンの遷移
    BOSS　初のコミット　の巻
    SceneMoveスクリプトを使えばハンマーがコリジョンから離れた瞬間にシーンが切り替わるよ

---
+++ Assets/Script/SceneMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    public string scenemoveNextScene;//この変数にSceneの名前を打ち込む
+
+    void OnCollisionExit(Collision other)//特定のコリジョンから離れると発動
+    {
+        if (other.gameObject.CompareTag("Hammer"))//特定のTagの場合
+        {
+            SceneManager.LoadScene(scenemoveNextScene);//次のシーンに吹っ飛ぶ
+        }
+    }
+}
