1b8fa38fac2c7945bc6b9a651c32008f2971dfe5
Mebu/YukiNarayama <mebumitiru@gmail.com>
2019-06-19 11:57:02 +0900

    developを更新　
    追加ファイル
    MenueSwitch,prefab Menueをprefab化して保守性を高めた
    PlayerDevMasChange.prefab & 同.cs OculusGo実機とエミュレーターの切り替え用
    
    変更ファイル
    各.unityファイル　MenueSwitchとPlayer切り替えのため

---
+++ Assets/Script/PlayerDevMasChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDevMasChange : MonoBehaviour
+{
+    public bool oculusGo ;
+    public GameObject oculusGoplayer;
+    public GameObject mousePlayer;
+    void Awake()
+    {
+        if(oculusGo)
+        {
+            oculusGoplayer.SetActive(true);
+            mousePlayer.SetActive(false);
+        }else
+        {
+            oculusGoplayer.SetActive(false);
+            mousePlayer.SetActive(true);
+        }
+    }
+}
