54878bd0a240cc7e2b11a0e48aae4a71c501ab94
Mebu/YukiNarayama <mebumitiru@gmail.com>
2019-07-03 15:26:56 +0900

    音を切れないように変更

---
+++ Assets/Script/TitleSoundBOX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TitleSound
+{
+    public class TitleSoundBOX : MonoBehaviour
+    {
+        private static AudioSource audioSouse;
+        public bool dontDestroyEnabled = true;
+        void Start ()
+        {
+            if (dontDestroyEnabled)
+            {
+                // Sceneを遷移してもオブジェクトが消えないようにする
+                DontDestroyOnLoad (this);
+            }
+            audioSouse = GetComponent<AudioSource> ();
+        }
+
+        public static void OnTitleSound ()///タイトルの音再生
+        {
+            audioSouse.Play ();
+        }
+
+    }
+}
