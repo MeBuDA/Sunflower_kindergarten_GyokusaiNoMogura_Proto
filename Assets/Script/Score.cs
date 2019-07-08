6ce9e382b5316455b4a0e663ff0b68dec16b06ef
Ryo Hashiguchi <m0118206a2@edu.teu.ac.jp>
2019-07-08 12:12:30 +0900

    スコア実装
    ・各モグラごとのスコアの設定
    ・スコアを仮のcanvasに表示

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private string thisObjectName;
+
+    private int thisObjectScore;
+
+    private GameObject moguraObject;
+
+    public string nameOfMoguraObject;//この変数にmoguraObjectの名前を打ち込む(めんどいのはわかるけどこれだけはお願い♥)
+
+    private Animator animator;
+
+    private bool attackFlag;
+
+    void Start()
+    {
+        thisObjectName = this.gameObject.name; //付けているオブジェクトの名前を参照
+
+        //モグラの種類によって叩いたときのスコアを設定(始)
+        if (thisObjectName == "Mogura")
+        {
+            thisObjectScore = 100;
+        }
+        else if (thisObjectName == "BossMogura ")
+        {
+            thisObjectScore = 500;
+        }
+        else if (thisObjectName == "GoldMogura ")
+        {
+            thisObjectScore = 1000;
+        }
+        else if (thisObjectName == "Ojisan")
+        {
+            thisObjectScore = -400;
+        }
+        //モグラの種類によって叩いたときのスコアを設定(終)
+
+        moguraObject = GameObject.Find(nameOfMoguraObject); //MoguraObjectを取得
+
+        animator = moguraObject.GetComponent<Animator>(); //Animatorを取得
+
+        attackFlag = true; //攻撃可能状態
+    }
+
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("MoguraKanState")) //モグラ(全種)が待機状態のAnimationの場合
+        {
+            attackFlag = true; //攻撃可能状態
+        }
+    }
+    void OnTriggerEnter(Collider other) //特定のコリジョンに触れた瞬間発動
+    {
+        if (other.gameObject.CompareTag("Hammer")) //特定のTagの場合
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("MoguraKanOut")  ||  //条件文(始)
+                animator.GetCurrentAnimatorStateInfo(0).IsName("MoguraKanIn")   ||
+                animator.GetCurrentAnimatorStateInfo(0).IsName("BossMoguraOut") ||
+                animator.GetCurrentAnimatorStateInfo(0).IsName("BossMoguraIn")  ||  //モグラ(全種)が待機状態でないAnimetionの場合
+                animator.GetCurrentAnimatorStateInfo(0).IsName("GoldMoguraOut") ||
+                animator.GetCurrentAnimatorStateInfo(0).IsName("GoldMoguraIn")  ||
+                animator.GetCurrentAnimatorStateInfo(0).IsName("OjisanOut")     ||
+                animator.GetCurrentAnimatorStateInfo(0).IsName("OjisanIn")        ) //条件文(終)
+            {
+                if (attackFlag == true) //攻撃可能状態の場合
+                {
+                    FindObjectOfType<ScoreManager>().AddScore(thisObjectScore); //ScoreManagerスクリプトのthisObjectScoreメソッド起動
+                    attackFlag = false; //攻撃不可状態
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreLabel;
+    public float scoreCount;
+
+    void Start()
+    {
+        scoreCount = 0; //スコアリセット
+
+        scoreLabel.text = "Score : " + scoreCount; //スコア表示
+    }
+
+    public void AddScore(int addPoint) //Scoreスクリプトから起動
+    {
+        scoreCount = scoreCount + addPoint; //スコア加算
+    }
+
+    void Update()
+    {
+        if (scoreCount < 0) //スコアが0未満になった時に発動(スコア0の時に減点したとき用)
+        {
+            scoreCount = 0;
+        }
+
+        scoreLabel.text = "Score : " + scoreCount; //スコア表示
+    }
+}
