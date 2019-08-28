19e31dd6754bd640ec7108a3112e8b975d859f9e
Ryo Hashiguchi <m0118206a2@edu.teu.ac.jp>
2019-08-28 23:57:26 +0900

    モグラロケットver2
    以下の機能を実装
    ・ロケット砲台が1分後に出現するようになった
    ・ロケットモグラが発射前に戻る時におかしな挙動をするのを修正
    
    なお、10yenが指摘したバグも修正済み

--- Assets/Script/RocketMogura.cs
+++ Assets/Script/RocketMogura.cs
@@ -10,14 +10,40 @@ namespace RocketMoguraPosition
     {
         private Animator rocketAnimator;
 
+        [SerializeField] float rocketUpTime; //モグラロケットが起動するまでの時間(秒)
+        private bool rocketUpFlag;
+
         void Awake()
         {
             rocketAnimator = this.GetComponent<Animator>();
         }
 
+        void Start()
+        {
+            rocketUpFlag = false;
+            Invoke("RocketUp", rocketUpTime - 3.0f);
+        }
+
+        void RocketUp()
+        {
+            rocketAnimator.SetTrigger("RocketMoguraKanUp");
+            Invoke("RocketUpFinish", 3.0f);
+        }
+
+        void RocketUpFinish()
+        {
+            rocketUpFlag = true;
+        }
+
         public void RocketMoguraFire()
         {
-            rocketAnimator.SetTrigger("RocketMoguraFire");
+            if(rocketAnimator.GetCurrentAnimatorStateInfo(0).IsName("RocketMoguraKanState")) //このif文があるとロケットモグラが戻った直後に発射されなくなる
+            {
+                if (rocketUpFlag == true)
+                {
+                    rocketAnimator.SetTrigger("RocketMoguraFire");
+                }
+            }
         }
     }
 }
