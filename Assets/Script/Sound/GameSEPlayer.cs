0c5481c7be8d32ef69b750174918d93e28b5be79
10yen <m011825212@edu.teu.ac.jp>
2019-09-02 02:17:38 +0900

    もぐらロケットの音(発射、叩く)
    もぐらロケットが発射される音と飛んできたもぐらを打ち返す音を実装
    なんか和風
    
    <追加>
    ・MoguraRocketHitSound.cs
    ・FireMog_pri01.wav
    ・HitMogRocket_pri01.wav
    
    <変更>
    ・RocketMoguraFire.anim
    LoopTimeをオフに変更
    ・RocketMoguraObject.controller
    RocketMoguraFireの頭にEvent (GameSEPlayer.PlaySE3D)を設置
    ・RocketMogura.prefab
    GameSEPlayer、MoguraRocketHitSoundの適用
    MoguraHitSoundの削除
    ・RocketMoguraObject.prefab
    GameSEPlayer、AudioSourceの適用

---
+++ Assets/Script/Sound/GameSEPlayer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SoundSystem{
+    //このスクリプトをゲームオブジェクトに適用した際、AudioSourceも適用されるように設定
+    [RequireComponent(typeof(AudioSource))]
+    public class GameSEPlayer : MonoBehaviour{
+        //コンポーネント
+        public AudioSource gameSE;
+
+        //SEリスト
+        public List<AudioClip> gameSEList = new List<AudioClip>();
+
+        //リストにAudioClipが一つしか登録されていないときに使う
+        //AudioClipが複数割り当てられている場合、最初の音を再生することも一応できる
+        public void PlaySE3D(float playPitch = 1f){
+            if(gameSEList.Count() != 1){
+                Debug.Log("Elements of gameSEList is not one");
+            }
+            
+            if(gameSEList[0] == null){
+                Debug.Log("AudioClip Not Found");
+            }
+            else{
+                gameSE.pitch = playPitch;
+                gameSE.spatialBlend = 1f;
+                gameSE.clip = gameSEList[0];
+
+                gameSE.Play();
+            }
+        }
+
+        //Play3DのOneShot版
+        public void PlaySEOneShot3D(float playPitch = 1f){
+            if(gameSEList.Count() != 1){
+                Debug.Log("Elements of gameSEList is not one");
+            }
+            
+            if(gameSEList[0] == null){
+                Debug.Log("AudioClip Not Found");
+            }
+            else{
+                gameSE.pitch = playPitch;
+                gameSE.spatialBlend = 1f;
+
+                gameSE.PlayOneShot(gameSEList[0]);
+            }
+        }
+
+        //Play3Dの先着優先バージョン
+        public void PlaySE3D_PrioritizePrevious(float playPitch = 1f){
+            if(gameSEList.Count() != 1){
+                Debug.Log("Elements of gameSEList is not one");
+            }
+            
+            if(gameSEList[0] == null){
+                Debug.Log("AudioClip Not Found");
+            }
+            else{
+                if(!gameSE.isPlaying){
+                    gameSE.pitch = playPitch;
+                    gameSE.spatialBlend = 1f;
+                    gameSE.clip = gameSEList[0];
+
+                    gameSE.Play();
+                }
+            }
+        }
+
+        //リストから指定した名前のAudioClipを呼び出し再生
+        public void PlaySEOneShot3D(string clipName, float playPitch = 1f){
+            if(gameSEList.FirstOrDefault(clip => clip.name == clipName) == null){
+                Debug.Log(clipName + " Not Found");
+            }
+
+            else{
+                gameSE.pitch = playPitch;
+                gameSE.spatialBlend = 1f;
+
+                gameSE.PlayOneShot(gameSEList.FirstOrDefault(clip => clip.name == clipName));
+            }
+        }
+
+        //リストから指定した番号のAudioClipを呼び出し再生
+        public void PlaySEOneShot3D(int clipNum, float playPitch = 1f){
+            if(gameSEList[clipNum] == null){
+                Debug.Log(clipNum.ToString() + " Not Found");
+            }
+            else{
+                gameSE.pitch = playPitch;
+                gameSE.spatialBlend = 1f;
+
+                gameSE.PlayOneShot(gameSEList[clipNum]);
+            }
+        }
+
+        //適用されたAudioSourceの初期化処理
+        public void Reset(){
+            gameSE = GetComponent<AudioSource>();
+            gameSE.playOnAwake = false;
+        }
+    }
+}
