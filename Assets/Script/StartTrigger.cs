ed9b4e5b6ed689f0fea80d659175e5db2ce3bd29
Ryo Hashiguchi <m0118206a2@edu.teu.ac.jp>
2019-09-11 19:03:20 +0900

    エフェクト実装
    ※スイッチのエフェクトだけちょっと違和感あるから確認してみて

--- Assets/Script/StartTrigger.cs
+++ Assets/Script/StartTrigger.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class StartTrigger : MonoBehaviour
 {
-	
-	void OnCollisionExit(Collision other)
+    [SerializeField] GameObject[] hitHammerEffects; //ハンマーが当たった時のエフェクト
+
+    void OnCollisionExit(Collision other)
 	{
 		GameObject Switch = GameObject.Find("StartSwitch");//objectの取得
 		if (other.gameObject.CompareTag("Hammer"))//ハンマーに当たった時
 		{
-			Switch.gameObject.transform.Translate(-1000,-1000,-1000);//StartSwitchを画面外に吹っ飛ばす
+            for (int i = 0; i < hitHammerEffects.Length; i++)
+            {
+                Instantiate(hitHammerEffects[i], this.transform.position, this.transform.rotation); //このオブジェクトと同じ場所にエフェクトを生成
+            }
+            Switch.gameObject.transform.Translate(-1000,-1000,-1000);//StartSwitchを画面外に吹っ飛ばす
 			//Debug.Log("消えたよ");
 		}
 	}
