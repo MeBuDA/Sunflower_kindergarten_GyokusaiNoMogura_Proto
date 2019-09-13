ae18b18fcb52fd03617053500c805fbe463abac6
10yen <m011825212@edu.teu.ac.jp>
2019-09-14 04:51:21 +0900

    もぐらを叩く音(修正)
    もぐらをいぢめるのは一回まで！！
    
    <変更>
    ・MoguraHitSound.cs
    Animatorを取得するように変更
    もぐらを一回叩いたら、戻ってくるまでは叩いても何も起こらないように修正
    ボスのコード、もろたで工藤！！

--- Assets/Script/Sound/MoguraHitSound.cs
+++ Assets/Script/Sound/MoguraHitSound.cs
@@ -7,18 +7,29 @@ public class MoguraHitSound : MonoBehaviour{
     //もぐら
     public HammerHitJudge[] mogura;
 
+    //animator
+    public Animator mogAnimator;
+
     //SoundSystem
     public GameSEPlayer mogHitSE;
 
+    //flag
+    private bool attackFlag;
+
     void Update(){
+        if (mogAnimator.GetCurrentAnimatorStateInfo(0).IsName("MoguraKanState")){
+            attackFlag = true;                                                      //攻撃可能状態
+        }
+
         //HammerHitJudgeからもぐらにハンマーが当たったサインを受け取る
         for(int i = 0; i < mogura.Length; i++){
             if(mogura[i] == null){
                 Debug.Log("mogura No." + i.ToString() + " Not Found");
             }
-            else if(mogura[i].EnterFlag){
+            else if(mogura[i].EnterFlag && attackFlag && !(mogAnimator.GetCurrentAnimatorStateInfo(0).IsName("MoguraKanState"))){
                 //Debug.Log(i.ToString());
                 mogHitSE.PlaySEOneShot3D(i);
+                attackFlag = false;
             }
         }
     }
