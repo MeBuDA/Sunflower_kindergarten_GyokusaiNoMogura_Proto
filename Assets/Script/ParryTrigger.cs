54a8c08a8bf30b138c24cefa8aa2f89e9644a7b9
matunga <m0118239@teu.edu.ne.jp>
2019-09-16 23:12:21 +0900

    NewParry
    Parryの実装と偽フェイントの二つ目の理由のAttackのトリガーが残り続けるバグを修正

---
+++ Assets/Script/ParryTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParryTrigger : MonoBehaviour
+{
+
+	public Animator MoguraAttackAni;
+
+	void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.CompareTag("Hammer"))
+		{
+			MoguraAttackAni.SetTrigger("Parry");
+			Debug.Log("Parry");
+		}
+	}
+}
