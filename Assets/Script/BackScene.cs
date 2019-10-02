a3b95cf026a9743722a3de2fcd86dae7277f804b
Mebu/YukiNarayama <mebumitiru@gmail.com>
2019-10-02 16:04:02 +0900

    戻るボタンってやっぱ欲しくね？

---
+++ Assets/Script/BackScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackScene : MonoBehaviour
+{
+    public string backSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+#if !UNITY_EDITOR
+    void Update()
+    {
+       if(OVRInput.GetDown(OVRInput.Button.Back))
+       {
+           SceneManager.LoadScene(backSceneName);
+       } 
+    }
+#endif
+void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+           SceneManager.LoadScene(backSceneName);
+       } 
+    }
+}
